4cb275d5dcdd61349c6f2b2ee08f58dcbbfcb9ff
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2013-11-26 08:19:57 -0800

    Initial simple templating.

---
+++ test.js
@@ -0,0 +1,25 @@
+var assert = require('assert')
+  , jigplate = require('./index')
+
+test(
+  'hello {{ world }} how is your {{ weekday }} going?',
+  { world: 'dude', weekday: 'tuesday' },
+  'hello dude how is your tuesday going?'
+)
+
+test(
+  '{{first}}{{second}}',
+  { first: '{{ second }}', second: '{{ first }}' },
+  '{{ second }}{{ first }}'
+)
+
+test(
+  '{{ whoops }} is my favorite {{ a b }}{{',
+  {},
+  ' is my favorite {{ a b }}{{'
+)
+
+function test(jig, context, expected) {
+  var tpl = jigplate(jig)
+  assert.equal(tpl(context), expected)
+}
