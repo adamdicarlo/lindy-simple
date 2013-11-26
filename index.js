30dac7197ed1291abfd41b0051770941acfb7ee3
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2013-11-26 08:49:14 -0800

    Remove unnecessary ternary expression.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function jigplate(jig) {
   return function(context) {
     return jig.replace(/\{\{\s*(\w+)\s*\}\}/g, function(match, p1) {
-      return context[p1] ? context[p1] : ''
+      return context[p1] || ''
     })
   }
 }
